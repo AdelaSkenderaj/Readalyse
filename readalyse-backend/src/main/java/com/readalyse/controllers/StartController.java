4a8008a71699998242832d6776f1a5ca62eb867c
AdelaSkenderaj <adelaskenderaj@gmail.com>
2025-03-21 22:57:00 +0100

    removes old implementation on file retrieval and information extraction

--- readalyse-backend/src/main/java/com/readalyse/controllers/StartController.java
+++ readalyse-backend/src/main/java/com/readalyse/controllers/StartController.java
@@ -2,12 +2,8 @@ package com.readalyse.controllers;
 
 import com.readalyse.book.BookEntity;
 import com.readalyse.book.BookRepository;
-import com.readalyse.dataImport.FileRetrieval;
-import com.readalyse.dataImport.InformationExtraction;
 import com.readalyse.entities.ReadabilityScoresEntity;
 import com.readalyse.services.BookService;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,40 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class StartController {
 
-  private final InformationExtraction informationExtraction;
-
   private final BookRepository bookRepository;
 
-  private final FileRetrieval fileRetrieval;
-
-  private static String basePath = "C:/Users/Dela/test";
   private final BookService bookService;
-
-  @GetMapping("/book")
-  public void getBook() {
-    informationExtraction.extractInformation(new File(basePath));
-  }
-
-  /* @GetMapping("/retrieveData")
-  public void startAnalysis() throws IOException, ParserConfigurationException, SAXException {
-    fileRetrieval.getRdfData();
-  }*/
-
-  @GetMapping("/retrieveData2")
-  public void retrieveFiles() throws IOException {
-    fileRetrieval.getRdfData();
-  }
 
   @GetMapping("/books")
   public List<BookEntity> findAll() {
     Page<BookEntity> books = bookRepository.findAll(PageRequest.of(0, 100));
     return books.getContent();
   }
-
-  /* @GetMapping("/book/{bookId}")
-  public BookEntity getBook1(@PathVariable Long bookId) {
-    return informationExtraction.getBook(String.valueOf(bookId));
-  }*/
 
   @GetMapping("/book/{bookId}/textbook")
   public String getText(@PathVariable Long bookId) {
